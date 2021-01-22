0e8dbbc6f0cae60cd92e6a048e47c1292c1d91d1
Abdullah AL Shohag <HackerShohag@outlook.com>
2021-01-22 10:48:32 +0600

    updating kernel, version E1M-068R-0-00WW-B01

--- arch/arm/mach-msm/fih/fih_sd_status.h
+++
@@ -1,8 +0,0 @@
-#ifndef __FIH_SD_STATUS_H
-#define __FIH_SD_STATUS_H
-
-#define FIH_SD_STATUS_SIZE  (64)
-
-void fih_sd_status_setup(char *info);
-
-#endif /* __FIH_SD_STATUS_H */
--- arch/arm/mach-msm/fih/fih_touch.h
+++
@@ -1,6 +0,0 @@
-#ifndef __FIH_TOUCH_H
-#define __FIH_TOUCH_H
-
-void fih_info_set_touch(char *info);
-
-#endif /* __FIH_TOUCH_H */
